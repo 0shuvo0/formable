67ac4d209f64c8c5231e29519ebe12a74de35017
0shuvo0 <seriouslolz1@gmail.com>
2021-07-31 14:56:09 +0600

    simple css update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,7 @@ function Home(){
         <div className="section">
             <img src={banner} alt="banner" />
             <div className="content">
-                <h1>Create, Share forms easily</h1>
+                <h1>Create, Share <span>forms</span> easily</h1>
                 <p>Formale lets you create forms super simply. All you need to do is create a free account and you'll be all set. You can share the link of your form with others and see thier submissions. It's suitable for online MCQ exam and for job recruitments</p>
                 <Link to="/create" className="btn">get started</Link>
             </div>
